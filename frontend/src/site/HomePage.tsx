0030ef7fa8c34584f7d7b2ba9213ae0c9a5a0c4c
nkg <nkg753@gmail.com>
2020-03-09 19:56:54 +0100

    Short cut to classify on HomePage and DetailsPage

--- frontend/src/site/HomePage.tsx
+++ frontend/src/site/HomePage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Jumbotron } from "react-bootstrap";
 import { useI18n } from "../utils";
+import { Link } from "react-router-dom";
 
 const HomePage: React.FC = () => {
   const _ = useI18n();
@@ -11,9 +12,15 @@ const HomePage: React.FC = () => {
       <p className="lead">{_("home.lead")}</p>
       <hr className="my-4" />
       <p>{_("home.description")}</p>
-      <a className="btn btn-primary btn-lg" href="https://credo.science/" role="button" target="_blank" rel="noopener noreferrer">
-        {_("home.website")}
-      </a>
+      <div className="text-center">
+        <Link to="/classify" className="btn btn-lg btn-success mb-4">
+          {_("home.classify")}
+        </Link>
+        <br />
+        <a className="btn btn-primary btn-lg" href="https://credo.science/" role="button" target="_blank" rel="noopener noreferrer">
+          {_("home.website")}
+        </a>
+      </div>
     </Jumbotron>
   );
 };
--- frontend/src/site/user/DetailsPage.tsx
+++ frontend/src/site/user/DetailsPage.tsx
@@ -3,6 +3,7 @@ import { AppContext } from "../../context/AppContext";
 import { Button, Card, Container } from "react-bootstrap";
 import { useApiClient } from "../../api/api";
 import { useI18n } from "../../utils";
+import { Link } from "react-router-dom";
 
 const containerStyle = { maxWidth: 540, marginTop: 60 };
 
@@ -21,8 +22,11 @@ const DetailsPage: React.FC = () => {
     <Container style={containerStyle}>
       <Card>
         <Card.Body>
-          <Card.Title className="text-center">{`${user?.first_name} ${user?.last_name} (${user?.username})`}</Card.Title>
+          <Card.Title className="text-center">{user?.username}</Card.Title>
           <Card.Subtitle className="mb-2 text-muted text-center">{`${_("user.scores")} ${user?.score}`}</Card.Subtitle>
+          <Link to="/classify" className="btn btn-block btn-success">
+            {_("user.classify")}
+          </Link>
           <Button variant="primary" onClick={doLogout} block disabled={isLoading}>
             {_("user.logout")}
           </Button>
@@ -33,3 +37,5 @@ const DetailsPage: React.FC = () => {
 };
 
 export default DetailsPage;
+
+// <Card.Title className="text-center">{`${user?.first_name} ${user?.last_name} (${user?.username})`}</Card.Title>
