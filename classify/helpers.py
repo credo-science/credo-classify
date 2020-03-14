f8d7ee542b13597e323a2fa57244bad357318102
nkg <nkg753@gmail.com>
2020-03-14 20:56:10 +0100

    Fix: query for next hit to classify

--- classify/helpers.py
+++ classify/helpers.py
@@ -7,19 +7,22 @@ from database.models import Detection
 from users.models import User
 
 
-def find_unclassified_by_user(user: User, count=1, qs: QuerySet = None) -> List[Detection]:
+def find_unclassified_by_user(user: User, count=1, own_qs: QuerySet = None) -> List[Detection]:
     ret = []
-    ids = []
+    tries = 100
 
-    qs = (qs or Detection.objects.all()).filter(mime__isnull=False).order_by('score', 'random')
+    while len(ret) < count and tries > 0:
+        ids = []
+        qs = (own_qs or Detection.objects.all()).filter(mime__isnull=False).order_by('score', 'random')[:count]
 
-    for d in qs:
-        if Classified.objects.filter(user=user, detection=d).count() == 0:
-            ret.append(d)
-            ids.append(d.id)
-        if len(ret) >= count:
-            break
+        for d in qs:
+            if Classified.objects.filter(user=user, detection=d).count() == 0:
+                ret.append(d)
+                ids.append(d.id)
+            if len(ret) >= count:
+                break
 
-    Detection.objects.filter(id__in=ids).update(score=F('score') + 1)
+        Detection.objects.filter(id__in=ids).update(score=F('score') + 1)
+        tries -= 1
 
     return ret
