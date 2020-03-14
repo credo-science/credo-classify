4e6476b411581ddccfe972895c7fb602d7a939ad
nkg <nkg753@gmail.com>
2020-03-14 18:51:51 +0100

    Fix: new URL

--- bash_management/settings.sh
+++ bash_management/settings.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export CC_HOST='http://localhost:8000/'  # with / at the end!
+export CC_HOST='http://localhost:8000/user-interface/classification/'  # with / at the end!
 export CC_USER='nkg'
 export CC_PASS='nkg'
 export CC_TOKEN='0000000000000000000000000000000000000000'  # please obtain by 01_obtain_token.sh
