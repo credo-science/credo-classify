f6abcb54f2fbfbab9009e3be4095d8337920fb61
nkg <nkg753@gmail.com>
2020-03-30 13:19:07 +0200

    Reconstruction of cutoff values and progress logs

--- database/management/commands/import.py
+++ database/management/commands/import.py
@@ -5,7 +5,7 @@ from io import StringIO, BytesIO
 import json
 from PIL import Image
 
-from typing import Any, List
+from typing import Any, List, Tuple
 
 from django.core.management import BaseCommand
 
@@ -26,22 +26,25 @@ def get_and_set(obj: dict, key: Any, default: Any) -> Any:
     return o
 
 
-def store_png(path: List[str], _id: int, frame_decoded: bytes) -> None:
-    # TODO: configurable PNG storage (yes/no) and destination directory
+def join_tuple(tpl: Tuple[int, int]) -> str:
+    return '%dx%d' % tpl
+
+
+def store_png(root: str, path: List[str], _id: int, frame_decoded: bytes) -> None:
     dirs = '/'.join(path)
-    p = "/tmp/hits/%s" % dirs
+    p = "%s/%s" % (root, dirs)
     fn = '%s/%d.png' % (p, _id)
     pathlib.Path(p).mkdir(parents=True, exist_ok=True)
     with open(fn, 'wb') as f:
         f.write(frame_decoded)
 
 
-def get_resolution_key(d: dict) -> str:
-    return '%dx%d' % (d.get('width'), d.get('height'))
+def get_resolution_key(d: dict) -> Tuple[int, int]:
+    return d.get('width'), d.get('height')
 
 
-def get_xy_key(d: dict) -> str:
-    return '%dx%d' % (d.get('x'), d.get('y'))
+def get_xy_key(d: dict) -> Tuple[int, int]:
+    return d.get('x'), d.get('y')
 
 
 def append_to_frame(image: Image, detection: dict):
@@ -59,18 +62,26 @@ def append_to_frame(image: Image, detection: dict):
     fx = 0
     fy = 0
 
-    for cy in range(height):
-        for cx in range(width):
-            g = gray.getpixel((cx, cy))
-            if mg < g:
-                mg = g
-                fx = cx
-                fy = cy
+    if width == height:
+        fx = width // 2
+        fy = height // 2
+    else:
+        detection['edge'] = True
+        for cy in range(height):
+            for cx in range(width):
+                g = gray.getpixel((cx, cy))
+                if mg < g:
+                    mg = g
+                    fx = cx
+                    fy = cy
 
     image.paste(hit_img, (x - fx, y - fy, x - fx + width, y - fy + height))  # , mask)
+
+    # fix bug in early CREDO Detector App: black filled boundary 1px too large
     image.paste(image.crop((x - fx + width - 1, y - fy, x - fx + width, y - fy + height)), (x - fx + width, y - fy, x - fx + width + 1, y - fy + height))
     image.paste(image.crop((x - fx, y - fy + height - 1, x - fx + width, y - fy + height)), (x - fx, y - fy + height, x - fx + width, y - fy + height + 1))
     image.paste(image.crop((x - fx + width - 1, y - fy + height - 1, x - fx + width, y - fy + height)), (x - fx + width, y - fy + height, x - fx + width + 1, y - fy + height + 1))
+
     detection['crop_x'] = x - fx
     detection['crop_y'] = y - fy
     detection['crop_size'] = (width, height)
@@ -92,9 +103,15 @@ class Command(BaseCommand):
 
     by_device_and_minute = {}  # store objects by device and detection minute and timestamp
     datatype = None
+    out_dir = None
 
     def add_arguments(self, parser):
         parser.add_argument('-d', '--datatype', help='data to import from input: users, devices, teams, hits or pings')
+        parser.add_argument('-o', '--out-dir', help='path to store debug data. For hits: png files accepterd, rejected and suspicious (but inserted)', default='')
+
+    def store_png(self, path: List[str], _id: int, frame_decoded: bytes) -> None:
+        if self.out_dir:
+            store_png(self.out_dir, path, _id, frame_decoded)
 
     def parse_object(self, s: str) -> None:
         """
@@ -134,6 +151,7 @@ class Command(BaseCommand):
         hot_pixels = {}  # counting the same XY by device_id, frame size, X value and Y value
 
         # build: hot_pixels
+        print('Build hot pixel map...')
         for kd, v in self.by_device_and_minute.items():
             hpd = {}
             for km, minute in v.items():
@@ -143,53 +161,77 @@ class Command(BaseCommand):
                         xyk = get_xy_key(d)
                         r[xyk] = r.get(xyk, 0) + 1
             hot_pixels[kd] = hpd
+        print('... hot pixel map done')
+
+        # reconstruction the fill by black cropped frame in CREDO Detector app v2
+        print('Reconstruction cleaned image area after cutoff another hit in the same image frame (CREDO Detector app issue)...')
+        for kd, v in self.by_device_and_minute.items():
+            for km, minute in v.items():
+                for kt, timestamp in minute.items():
+                    if len(timestamp) > 1:
+                        image = Image.new('RGBA', (timestamp[0].get('width'), timestamp[0].get('height')), (0, 0, 0))
+                        edge = 'no_edge'
+                        for d in timestamp:
+                            if d.get('edge'):
+                                edge = 'edge'
+                        for d in reversed(timestamp):
+                            frame_decoded = d.get('frame_decoded')
+                            append_to_frame(image, d)
+                            self.store_png(['recostruct', edge, str(kd), str(kt), 'orig'], d.get('id'), frame_decoded)
+                        for d in timestamp:
+                            replace_from_frame(image, d)
+                            self.store_png(['recostruct', edge, str(kd), str(kt)], d.get('id'), d.get('frame_decoded'))
+                        if self.out_dir:
+                            image.save('%s/recostruct/%s/%d/%d/frame.png' % (self.out_dir, edge, kd, kt))
+        print('... reconstruction done')
 
         # build: bulk
+        print('Filter artifacts by too often and hot pixels...')
         for kd, v in self.by_device_and_minute.items():
             hpd = hot_pixels[kd]
             for km, minute in v.items():
                 for kt, timestamp in minute.items():
-                    # fix: the fill by black cropped frame in CREDO Detector app v2
-                    if len(timestamp) > 1:
-                        image = Image.new('RGBA', (timestamp[0].get('width'), timestamp[0].get('height')), (0, 0, 0))
-                        for d in reversed(timestamp):
-                            append_to_frame(image, d)
-                        for d in timestamp:
-                            replace_from_frame(image, d)
-                        # image.save('/tmp/test.png')
                     for d in timestamp:
                         rk = get_resolution_key(d)
-                        xyk = get_xy_key(d)
+                        rk_str = join_tuple(rk)
 
-                        if len(minute) >= 10:
-                            store_png(['rejected', 'by_too_often', str(kd), str(km)], d.get('id'), d.get('frame_decoded'))
-                            store_png(['rejected', 'by_too_often'], d.get('id'), d.get('frame_decoded'))
-                        elif hpd[rk][xyk] >= 3:
-                            store_png(['rejected', 'by_hot_pixel', str(kd), rk, xyk], d.get('id'), d.get('frame_decoded'))
-                            store_png(['rejected', 'by_hot_pixel'], d.get('id'), d.get('frame_decoded'))
+                        xyk = get_xy_key(d)
+                        xyk_str = join_tuple(xyk)
+
+                        if hpd[rk][xyk] >= 3:
+                            self.store_png(['rejected', 'by_hot_pixel', str(kd), rk_str, xyk_str], d.get('id'), d.get('frame_decoded'))
+                            self.store_png(['rejected', 'by_hot_pixel', 'by_count', str(hpd[rk][xyk])], d.get('id'), d.get('frame_decoded'))
+                        elif len(minute) >= 10:
+                            self.store_png(['rejected', 'by_too_often', str(kd), str(km)], d.get('id'), d.get('frame_decoded'))
+                            self.store_png(['rejected', 'by_too_often', 'by_count', str(len(minute))], d.get('id'), d.get('frame_decoded'))
                         else:
-                            if len(minute) > 1:
-                                store_png(['suspicious', 'by_too_often', str(kd), str(km)], d.get('id'), d.get('frame_decoded'))
-                                store_png(['suspicious', 'by_too_often'], d.get('id'), d.get('frame_decoded'))
-                            elif hpd[rk][xyk] > 1:
-                                store_png(['suspicious', 'by_hot_pixel', str(kd), rk, xyk], d.get('id'), d.get('frame_decoded'))
-                                store_png(['suspicious', 'by_hot_pixel'], d.get('id'), d.get('frame_decoded'))
+                            if hpd[rk][xyk] > 1:
+                                self.store_png(['suspicious', 'by_hot_pixel', str(kd), rk_str, xyk_str], d.get('id'), d.get('frame_decoded'))
+                                self.store_png(['suspicious', 'by_hot_pixel', 'by_count', str(hpd[rk][xyk])], d.get('id'), d.get('frame_decoded'))
+                            elif len(minute) > 1:
+                                self.store_png(['suspicious', 'by_too_often', str(kd), str(km)], d.get('id'), d.get('frame_decoded'))
+                                self.store_png(['suspicious', 'by_too_often', 'by_count', str(len(minute))], d.get('id'), d.get('frame_decoded'))
                             else:
-                                store_png(['accepted', str(kd)], d.get('id'), d.get('frame_decoded'))
-                                store_png(['accepted'], d.get('id'), d.get('frame_decoded'))
+                                self.store_png(['accepted', str(kd)], d.get('id'), d.get('frame_decoded'))
+                                self.store_png(['accepted'], d.get('id'), d.get('frame_decoded'))
                             # TODO: make Detection object and append to bulk[]
+        print('... filter artifacts by too often and hot pixels done')
+
         # TODO: insert bulk[] to database
 
     def handle(self, *args, **options):
         self.datatype = options['datatype']
+        self.out_dir = options['out_dir']
 
         if self.datatype != 'hits':
             print('Other datatypes than hits is not supported yet')
 
+        print('Parsing objects from JSON provided in STDIN...')
+        count = 0
+
         stage = 0
         buff = None
-        while True:
-            line = stdin.read()
+        for line in stdin:
             for a in line:
                 if stage == 0:
                     if a == '[':
@@ -208,10 +250,14 @@ class Command(BaseCommand):
                         buff.close()
                         buff = None
                         stage = 1
+
+                        count += 1
+                        if count % 10000 == 0:
+                            print('... just parsed %d objects...' % count)
                     else:
                         buff.write(a)
-            if line == '':
-                break
 
+        print('... done, the %d objects was parsed' % count)
+        print('\nPostprocess of objects and insert to database:')
         if self.datatype == 'hits':
             self.parse_hits()
